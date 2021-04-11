577cadfbde60473e71c6b02c2f35c80e799c75ae
Sajanbir Singh <ssingh5@me.iitr.ac.in>
2021-04-11 21:02:34 +0530

    first commit

---
+++ src/Components/Fetchapi.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import axios from 'axios'
+
+const URL ="https://openweathermap.org/data/2.5/weather";
+const API_Key = "a08cbd99ad10761ab8c4f5c98ce42504"
+
+const fetch =()=>{
+    return(
+<>
+</>
+    )
+}
---
+++ src/Components/Tempapp.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import "./style.css";
+
+const Tempapp = () => {
+  const [city, setCity] = useState();
+  const [search, setSearch] = useState();
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=a08cbd99ad10761ab8c4f5c98ce42504`;
+      const response = await fetch(url);
+      const resJason = await response.json();
+      setCity(resJason.main);
+    };
+    fetchApi();
+  }, [search])
+
+  return (
+    <>
+      <div className="box">
+        <div className="inputData">
+          <input
+            type="search"
+            value={search}
+            className="inputField"
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          />
+        </div>
+
+        { ! city ? (
+          <p className="error">No Data Found</p>
+        ) : (
+          <div>
+            <div className="info">
+              <h2 className="location">
+                <i className="fas fa-street-view"></i>
+                {search}
+              </h2>
+              <h1 className="temp">{city.temp}*Cel </h1>
+              <h3 className="tempmin_max">Min : {city.temp_min}*Cel | Max : {city.temp_max}*Cel</h3>
+            </div>
+            <div className="wave1"></div>
+            <div className="wave2"></div>
+            <div className="wave3"></div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Tempapp;
